5c54ffe2484e3a84d14d62ab890f7b083472fc92
SwapnilPanchade <sspanchade113@gmail.com>
2025-04-17 17:52:41 +0530

    added mongoose.schema and model video for the video.ts file and exported the model "

--- src/models/Video.ts
+++ src/models/Video.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document } from "mongoose";
+import { number, string } from "zod";
 export type VidoeAccessLevel = "public" | "free" | "pro" | "admin";
 
 export interface IVideo extends Document {
@@ -15,3 +16,35 @@ export interface IVideo extends Document {
   createdAt: Date;
   updatedAt: Date;
 }
+
+const VideoSchema: Schema<IVideo> = new Schema(
+  {
+    title: { type: String, required: true, trim: true, index: true },
+    description: { type: String, trim: true },
+    duration: { type: Number }, //In seconds
+    order: { type: Number, default: 0 },
+    accessLevel: {
+      type: String,
+      enum: ["public", "free", "pro", "admin"],
+      default: "free",
+      required: true,
+      index: true,
+    },
+    muxAssetId: { type: String, unique: true, sparse: true, index: true }, // Asset ID is unique if present
+    muxPlaybackId: { type: String, index: true }, // Playback ID associated with the asset
+    muxProcessingStatus: {
+      type: String,
+      enum: ["preparing", "ready", "errored"],
+      default: "preparing",
+    },
+    thumbnailUrl: { type: String },
+    isPublished: { type: Boolean, default: false, index: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Video = mongoose.model<IVideo>("Video", VideoSchema);
+
+export default Video;
